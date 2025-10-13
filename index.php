9249d68dfb7e528231036509f58fa66a4ee1c8a0
iancorbett <iancorbett1324@gmail.com>
2025-10-12 21:27:30 -0700

    add connection string

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
+<?php
 $host = '127.0.0.1';
 $db   = 'millville_demo';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
+
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset"; //data-source-name
