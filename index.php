635c704e76f67d3400b700488fa473b3479be07c
iancorbett <iancorbett1324@gmail.com>
2025-10-12 21:28:18 -0700

    add to options aarray to help with resolvign errors

--- index.php
+++ index.php
@@ -7,3 +7,9 @@ $charset = 'utf8mb4';
 
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset"; //data-source-name
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
